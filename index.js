b0db1f7a227c18f732ff22b35d8d2f5e3d4a3f60
rainingchicken <154983686+rainingchicken@users.noreply.github.com>
2024-06-13 10:15:15 -0700

    added todo to todo list

--- index.js
+++ index.js
@@ -5,7 +5,18 @@ const button = document.getElementById("add-todo");
 // console.log(button);
 //fn to run when btn is clicked
 function handleAddTodo() {
-  console.log("button is clicked");
+  const ul = document.querySelector("ul");
+  console.log(ul);
+  //create an li to go into ul
+  const li = document.createElement("li");
+  //access the input el
+  //set the input text to li text
+  const input = document.querySelector("input");
+  li.textContent = input.value;
+  //console.log(input);
+  //append li item to ul as child el
+  ul.appendChild(li);
 }
+
 //add am event listener to my button
 button.addEventListener("click", handleAddTodo);
