bff702348998ab711372a0bb16e3a3412be08a26
rainingchicken <154983686+rainingchicken@users.noreply.github.com>
2024-06-13 14:25:14 -0700

    put all existing elments in body into empty div in body and styling

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+const input = document.querySelector("input");
+
+//create empty div to put todo in for styling
+//create empty div
+const parent = document.createElement("div");
+parent.innerHTML = "";
+parent.classList.add("parentDiv");
+
+//get all existing elements from html body
+const nodes = document.body.querySelectorAll("*");
+//convert to elements
+const elements = [...nodes];
+
+//append to doc body
+document.body.appendChild(parent);
+//append all body lements into empty div
+for (const el of elements) {
+  // console.log(el);
+  parent.appendChild(el);
+}
+
 //Add a todo
 
 //access the button element
@@ -11,8 +32,8 @@ function handleAddTodo() {
   const li = document.createElement("li");
   //access the input el
   //set the input text to li text
-  const input = document.querySelector("input");
   li.textContent = input.value;
+  input.value = ""; //clear input
   //console.log(input);
   //append li item to ul as child el
   ul.appendChild(li);
@@ -20,3 +41,18 @@ function handleAddTodo() {
 
 //add am event listener to my button
 button.addEventListener("click", handleAddTodo);
+
+//styling
+//body style
+Object.assign(document.body.style, { boxSizing: "border-box" });
+
+//parent div style
+Object.assign(parent.style, {
+  display: "flex",
+  flexDirection: "column",
+  backgroundColor: "red",
+  width: "50%",
+  width: "50%",
+  margin: "auto",
+  marginTop: "10%",
+});
