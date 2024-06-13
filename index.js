a1ef206f9661f30030de21e303f283723d6231f7
rainingchicken <154983686+rainingchicken@users.noreply.github.com>
2024-06-13 09:32:15 -0700

    added event listener to button

--- index.js
+++ index.js
@@ -2,3 +2,10 @@
 
 //access the button element
 const button = document.getElementById("add-todo");
+// console.log(button);
+//fn to run when btn is clicked
+function handleAddTodo() {
+  console.log("button is clicked");
+}
+//add am event listener to my button
+button.addEventListener("click", handleAddTodo);
